71581e0af9d45ce22e2fbba486e05af6192f79ea
junaid-repo <junaidraza3001@gmail.com>
2024-08-24 20:29:00 +0530

    eureka impl

--- src/main/java/com/food/resturants/ResturantsApplication.java
+++ src/main/java/com/food/resturants/ResturantsApplication.java
@@ -2,11 +2,13 @@ package com.food.resturants;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableDiscoveryClient
 public class ResturantsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ResturantsApplication.class, args);
